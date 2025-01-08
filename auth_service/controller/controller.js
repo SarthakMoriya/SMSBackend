6aac8356955811681019f21ba6797645d33a023e
SarthakMoriya <sarthak8544@gmail.com>
2025-01-08 21:51:02 +0530

    added auth token verification route

--- auth_service/controller/controller.js
+++ auth_service/controller/controller.js
@@ -1,4 +1,6 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 import {
   checkExistingUser,
   checkExistingUserLogin,
@@ -35,7 +37,6 @@ export const login = async (req, res) => {
     // Check if the account  exists
     const existingUserRes = await checkExistingUserLogin(email);
 
-    console.log(existingUserRes);
     // Hash the password
     const hashedPassword = await bcrypt.compare(
       password,
@@ -44,14 +45,53 @@ export const login = async (req, res) => {
     if (!hashedPassword) {
       throw new Error("Invalid credentials");
     }
-
+    let token = jwt.sign(
+      { teacherId: existingUserRes.body.id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXP }
+    );
     // Respond with the new user data
     return res.status(200).json({
       ...existingUserRes.body,
       password: null,
+      token,
     });
   } catch (error) {
     console.error(error);
     return res.status(500).json(error);
+  }
+};
+
+export const verifyToken = async (req, res) => {
+  try {
+    let token = req.headers["authorization"];
+    if (!token) {
+      return res.statu(404).json({
+        status: "Unauthorized",
+        code: 400,
+        message: "Invalid token or Expired Token",
+      });
+    }
+
+    let { iat, exp, teacherId } = jwt.verify(token, process.env.JWT_SECRET);
+    if (iat < exp) {
+      return res.status(200).json({
+        message: "Authentication Success",
+        code: 200,
+        status: "success",
+        body: teacherId,
+      });
+    } else {
+      throw new Error({
+        message: "Expired Session",
+        status: "fail",
+        code: 500,
+      });
+    }
+  } catch (error) {
+    let code = error.code || 500;
+    let message = error.message || "Internal Server Error";
+    let status = error.status || "fail";
+    return res.status(code).json({ message, code, status });
   }
 };
--- auth_service/routes/routes.js
+++ auth_service/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, signup } from "../controller/controller.js";
+import { login, signup, verifyToken } from "../controller/controller.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/verify",verifyToken)
 
 export default router;
