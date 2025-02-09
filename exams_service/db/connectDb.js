9b74278930d1a8e2f02fdd29453b4b34d8c4a883
SarthakMoriya <sarthak8544@gmail.com>
2025-02-09 11:57:51 +0530

    Add exam codes retrieval and caching functionality

--- exams_service/db/connectDb.js
+++ exams_service/db/connectDb.js
@@ -32,3 +32,18 @@ export const closeDbConnection = (connectSql) => {
   console.log("Closing DB connection...");
   connectSql.destroy();
 };
+
+
+// PROMISES SQL
+// Create a connection pool
+const pool = mysql.createPool({
+    host: 'localhost',  // Change this to your database host
+    user: 'root',       // Change this to your MySQL username
+    password: 'root', // Change this to your MySQL password
+    database: '',   // Change this to your database name
+    waitForConnections: true,
+    connectionLimit: 10,  // Maximum number of connections
+    queueLimit: 0
+}).promise();
+
+export default pool;
